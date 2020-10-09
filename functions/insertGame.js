1f9cfd831a54b66925db6419800e83bdbabc5883
Chris Wilhite <chris.wilhite@datastax.com>
2020-10-08 20:32:42 -0400

    step 2 solution

--- functions/insertGame.js
+++ functions/insertGame.js
@@ -1,6 +1,40 @@
+const astra = require("astra-js");
+
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ hello: "world" }),
-  };
+  let gameId;
+  let gamePayload;
+  try {
+    gameId = event.path.split("insertGame/")[1];
+    gamePayload = JSON.parse(event.body);
+  } catch (e) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "must provide a valid game ID" }),
+    };
+  }
+
+  const astraClient = await astra.createClient({
+    baseUrl: `https://${process.env.ASTRA_DB_ID}-${process.env.ASTRA_DB_REGION}.apps.astra.datastax.com`,
+    username: process.env.ASTRA_DB_USERNAME,
+    password: process.env.ASTRA_DB_PASSWORD,
+  });
+  const namespace = process.env.ASTRA_DB_KEYSPACE;
+  const collection = process.env.GAMES_COLLECTION;
+
+  try {
+    const res = await astraClient.put(
+      `/namespaces/${namespace}/collections/${collection}/${gameId}`,
+      gamePayload
+    );
+    return {
+      statusCode: 200,
+      body: JSON.stringify(res.jsonResponse),
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e),
+    };
+  }
 };
--- functions/insertGame.test.js
+++ functions/insertGame.test.js
@@ -1,7 +1,24 @@
 const insertGame = require("./insertGame");
+const faker = require("faker");
 
-it("should return a JSON response", async () => {
-  const response = await insertGame.handler();
-  const responseJson = JSON.parse(response.body);
-  expect(responseJson.hello).toBe("world");
+it("should create a game document", async () => {
+  const response = await insertGame.handler({
+    path: "/functions/insertGame/" + faker.helpers.replaceSymbols("????"),
+    body: '{"user":"me"}',
+  });
+  expect(response.statusCode).toBe(200);
+});
+
+it("shouldn't create a game document without a game id", async () => {
+  const response = await insertGame.handler({ path: "insertGame" });
+  expect(response.statusCode).toBe(400);
+});
+
+it("should return a 500 on error", async () => {
+  process.env.GAMES_COLLECTION = undefined;
+  const response = await insertGame.handler({
+    path: "/functions/insertGame/" + faker.helpers.replaceSymbols("????"),
+    body: '{"user":"me"}',
+  });
+  expect(response.statusCode).toBe(500);
 });
